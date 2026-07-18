71bb2d5584ab77b8eba33eb3bd3589eeeda4db64
yakovypg <yasha-pmp@mail.ru>
2026-07-18 12:33:22 +0300

    Update home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,54 +1,59 @@
 import "styles/pages/home-page.css";
 import "styles/pages/page.css";
+import "styles/services.css";
 
 import contactsConfig from "@config/contacts.json";
 import React from "react";
-import { Nav, Navbar } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
-import { LinkContainer } from "react-router-bootstrap";
-import { VIEW_PANELS } from "types";
-import { formatPhone, getRoutePath } from "utils";
+import { formatPhone } from "utils";
 
 export const HomePage: React.FC = () => {
   const { t } = useTranslation();
 
+  const servicesRaw: unknown = t("general.services", { returnObjects: true });
+  const services: string[] = Array.isArray(servicesRaw) ? servicesRaw as string[] : [];
+
   return (
     <div className="page-center-container">
       <div className="page-center-left-subcontainer">
-        <h3 className="page-title color-primary">{t("organization.name")}</h3>
+        <h3 className="page-title two-lines-title uppercase-title color-primary">
+          <span className="word1">{t("organization.name").split(" ")[0]}</span>
+          <span className="word2">
+            {t("organization.name").split(" ").slice(1).join(" ")}
+          </span>
+        </h3>
 
-        <Navbar className="my-0 py-0">
-          <div className="d-flex justify-content-center align-items-center">
-            <Nav className="align-items-center big-text color-secondary">
-              <LinkContainer
-                className="mx-0 px-0 page-nav-link-container"
-                to={getRoutePath(VIEW_PANELS.jurisprudence)}>
-                <Nav.Link>{t("title.jurisprudence")}</Nav.Link>
-              </LinkContainer>
-              <LinkContainer
-                className="mx-4 page-nav-link-container"
-                to={getRoutePath(VIEW_PANELS.developing)}>
-                <Nav.Link>{t("title.developing")}</Nav.Link>
-              </LinkContainer>
-            </Nav>
+        <p className="page-subtitle">{t("organization.slogan")}</p>
+
+        <div className="info-container">
+          <div className="info-item">
+            <ul className="service-list full-width-list normal-text">
+              {
+                services.map((service: string) => (
+                  <li key={service}>{service}</li>
+                ))
+              }
+            </ul>
           </div>
-        </Navbar>
 
-        <hr className="my-2 mb-3 w-100" />
+          <div className="vertical-line info-item" />
 
-        <h3 className="page-subtitle">{t("phrase.aboutUs")}</h3>
-        <p className="normal-text">{t("organization.about.p1")}</p>
-        <p className="normal-text">{t("organization.about.p2")}</p>
+          <div className="info-item">
+            <h3 className="page-subtitle about-title">{t("phrase.aboutUs")}</h3>
+            <p className="normal-text">{t("organization.about.p1")}</p>
+            <p className="normal-text">{t("organization.about.p2")}</p>
+          </div>
+        </div>
 
-        <hr className="my-2 w-100" />
-
-        <div className="contacts-container">
-          <a className="contacts-item big-text color-primary" href={`mailto:${contactsConfig.email}`}>
-            {contactsConfig.email}
-          </a>
-          <a className="contacts-item big-text color-primary" href={`tel:${contactsConfig.phone}`}>
-            {formatPhone(contactsConfig.phone)}
-          </a>
+        <div className="contacts-wrapper">
+          <div className="mt-4 contacts-container">
+            <a className="contacts-item big-text color-primary" href={`mailto:${contactsConfig.email}`}>
+              {contactsConfig.email}
+            </a>
+            <a className="contacts-item big-text color-primary" href={`tel:${contactsConfig.phone}`}>
+              {formatPhone(contactsConfig.phone)}
+            </a>
+          </div>
         </div>
       </div>
     </div>

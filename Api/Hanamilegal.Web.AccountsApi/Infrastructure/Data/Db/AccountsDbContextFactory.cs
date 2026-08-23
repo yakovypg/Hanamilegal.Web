ec57a3bd2d4c49fc69b35338b473b389ee3f7031
yakovypg <yasha-pmp@mail.ru>
2026-08-23 03:39:18 +0300

    Add factories for DbContext

---
+++ Api/Hanamilegal.Web.AccountsApi/Infrastructure/Data/Db/AccountsDbContextFactory.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Hanamilegal.Web.AccountsApi.Infrastructure.Data.Db;
+
+internal sealed class AccountsDbContextFactory
+    : IDesignTimeDbContextFactory<AccountsDbContext>
+{
+    public AccountsDbContext CreateDbContext(string[] args)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("ACCOUNTS_DB_CONNECTION_STRING")
+            ?? throw new InvalidOperationException("Connection string not found");
+
+        var optionsBuilder = new DbContextOptionsBuilder<AccountsDbContext>();
+        string? assemblyName = typeof(AccountsDbContext).Assembly.GetName().Name;
+
+        optionsBuilder.UseNpgsql(
+            connectionString,
+            t => t.MigrationsAssembly(assemblyName));
+
+        return new AccountsDbContext(optionsBuilder.Options);
+    }
+}
---
+++ Api/Hanamilegal.Web.ApplicationsApi/Infrastructure/Data/Db/ApplicationsDbContextFactory.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Hanamilegal.Web.ApplicationsApi.Infrastructure.Data.Db;
+
+internal sealed class ApplicationsDbContextFactory
+    : IDesignTimeDbContextFactory<ApplicationsDbContext>
+{
+    public ApplicationsDbContext CreateDbContext(string[] args)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("APPLICATIONS_DB_CONNECTION_STRING")
+            ?? throw new InvalidOperationException("Connection string not found");
+
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationsDbContext>();
+        string? assemblyName = typeof(ApplicationsDbContext).Assembly.GetName().Name;
+
+        optionsBuilder.UseNpgsql(
+            connectionString,
+            t => t.MigrationsAssembly(assemblyName));
+
+        return new ApplicationsDbContext(optionsBuilder.Options);
+    }
+}

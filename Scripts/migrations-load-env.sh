f7edff5caf81b5182338d6b2392622fec1c828ed
yakovypg <yasha-pmp@mail.ru>
2026-08-23 03:37:23 +0300

    Add script for loading environment variables for migrations

---
+++ Scripts/migrations-load-env.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [[ -n ${BASH_VERSION:-} ]]; then
+    SOURCE_FILE="${BASH_SOURCE[0]}"
+elif [[ -n ${ZSH_VERSION:-} ]]; then
+    SOURCE_FILE="${(%):-%N}"
+else
+    echo "error: shell not supported" >&2
+    exit 1
+fi
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "$SOURCE_FILE")" && pwd)"
+ENV_FILE="$SCRIPT_DIR/../.env"
+
+if [[ ! -f "$ENV_FILE" ]]; then
+    echo "error: .env file not found in '$ENV_FILE'" >&2
+    exit 1
+fi
+
+set -a
+source "$ENV_FILE"
+set +a
+
+export ACCOUNTS_DB_CONNECTION_STRING="Host=localhost;Port=12001;Database=${ACCOUNTS_DB_NAME};Username=${ACCOUNTS_DB_USER};Password=${ACCOUNTS_DB_PASSWORD}"
+export APPLICATIONS_DB_CONNECTION_STRING="Host=localhost;Port=12002;Database=${APPLICATIONS_DB_NAME};Username=${APPLICATIONS_DB_USER};Password=${APPLICATIONS_DB_PASSWORD}"

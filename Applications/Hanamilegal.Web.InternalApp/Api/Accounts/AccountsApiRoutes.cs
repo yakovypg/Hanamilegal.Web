02555b5033f3b90af2c8f2b9ccf2b081c3020de2
yakovypg <yasha-pmp@mail.ru>
2026-08-24 13:04:53 +0300

    Change routes configuration

--- Applications/Hanamilegal.Web.InternalApp/Api/Accounts/AccountsApiRoutes.cs
+++ Applications/Hanamilegal.Web.InternalApp/Api/Accounts/AccountsApiRoutes.cs
@@ -2,5 +2,5 @@ namespace Hanamilegal.Web.InternalApp.Api.Accounts;
 
 internal sealed class AccountsApiRoutes
 {
-    internal const string Login = "api/accounts/login";
+    internal const string Login = "/api/accounts/login";
 }
--- Applications/Hanamilegal.Web.InternalApp/Api/Applications/ApplicationsApiRoutes.cs
+++ Applications/Hanamilegal.Web.InternalApp/Api/Applications/ApplicationsApiRoutes.cs
@@ -2,5 +2,5 @@ namespace Hanamilegal.Web.InternalApp.Api.Applications;
 
 internal sealed class ApplicationsApiRoutes
 {
-    internal const string Search = "api/applications/search";
+    internal const string Search = "/api/applications/search";
 }

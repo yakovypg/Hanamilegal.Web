4e5aa9efaab6afc79747bc0aef2b03a5af1b5a30
yakovypg <yasha-pmp@mail.ru>
2026-09-04 08:49:55 +0300

    Fix bug with cached locale files in TechApp

--- Applications/Hanamilegal.Web.TechApp/src/i18n.ts
+++ Applications/Hanamilegal.Web.TechApp/src/i18n.ts
@@ -3,6 +3,8 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import HttpBackend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
+const packageVersion: string = import.meta.env.PACKAGE_VERSION ?? "dev";
+
 i18n
   .use(HttpBackend)
   .use(LanguageDetector)
@@ -15,7 +17,7 @@ i18n
     interpolation: { escapeValue: false },
     react: { useSuspense: false },
     backend: {
-      loadPath: "/locales/{{lng}}/{{ns}}.json"
+      loadPath: `/locales/{{lng}}/{{ns}}.json?v=${packageVersion}`
     }
   });
 
